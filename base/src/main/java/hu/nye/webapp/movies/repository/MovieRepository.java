042ca1ea8813c2f8f8d834876e09a575ce460f52
Bor Bálint <br.blnt10@gmail.com>
2023-03-29 14:33:58 +0200

    changes

--- base/src/main/java/hu/nye/webapp/movies/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.nye.webapp.movies.repository;
-
-import hu.nye.webapp.movies.entity.MovieEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
-}
