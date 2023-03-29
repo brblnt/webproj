dc37793be6c5daa5be586150617a656e7977a6ee
Bor Bálint <br.blnt10@gmail.com>
2023-03-29 14:29:17 +0200

    add project files

---
+++ base/src/main/java/hu/nye/webapp/movies/controller/MovieController.java
@@ -0,0 +1,84 @@
+package hu.nye.webapp.movies.controller;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import hu.nye.webapp.movies.dto.MovieDTO;
+import hu.nye.webapp.movies.exception.MovieValidationException;
+import hu.nye.webapp.movies.service.MovieService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/movies")
+public class MovieController {
+
+    private MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public List<MovieDTO> findAll() {
+        List<MovieDTO> movies = movieService.findAll();
+        return movies;
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<MovieDTO> findById(@PathVariable Long id) {
+        Optional<MovieDTO> movieDTO = movieService.findById(id);
+
+        ResponseEntity<MovieDTO> response;
+
+        if (movieDTO.isPresent()) {
+            response = ResponseEntity.ok(movieDTO.get());
+            // response = ResponseEntity.status(200).body(movieDTO.get());
+        } else {
+            // response = ResponseEntity.status(404).build();
+            response = ResponseEntity.notFound().build();
+        }
+
+        return response;
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.POST)
+    public ResponseEntity<MovieDTO> save(@RequestBody MovieDTO movieDTO) {
+        MovieDTO savedMovie = movieService.save(movieDTO);
+
+        return ResponseEntity.status(201).body(savedMovie);
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.PUT)
+    public ResponseEntity<MovieDTO> update(@Valid @RequestBody MovieDTO movieDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            List<String> errorList = new ArrayList<>();
+            for (FieldError fieldError : fieldErrors) {
+                errorList.add(fieldError.getDefaultMessage());
+            }
+
+            throw new MovieValidationException(errorList);
+        }
+
+        MovieDTO updatedMovie = movieService.update(movieDTO);
+
+        return ResponseEntity.status(200).body(updatedMovie);
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        movieService.delete(id);
+        return ResponseEntity.status(204).build();
+    }
+
+}
---
+++ base/src/main/java/hu/nye/webapp/movies/service/impl/MovieServiceImpl.java
@@ -0,0 +1,89 @@
+package hu.nye.webapp.movies.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import hu.nye.webapp.movies.dto.MovieDTO;
+import hu.nye.webapp.movies.entity.MovieEntity;
+import hu.nye.webapp.movies.exception.MovieNotFoundException;
+import hu.nye.webapp.movies.repository.MovieRepository;
+import hu.nye.webapp.movies.service.MovieService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieServiceImpl implements MovieService {
+
+    private MovieRepository movieRepository;
+    private ModelMapper modelMapper;
+
+    public MovieServiceImpl(MovieRepository movieRepository, ModelMapper modelMapper) {
+        this.movieRepository = movieRepository;
+        this.modelMapper = modelMapper;
+
+        MovieEntity movieEntity = new MovieEntity();
+        movieEntity.setTitle("Star Wars");
+        movieRepository.save(movieEntity);
+    }
+
+    @Override
+    public List<MovieDTO> findAll() {
+        List<MovieEntity> movies = movieRepository.findAll();
+
+        List<MovieDTO> result = new ArrayList<>();
+
+        for (MovieEntity movieEntity : movies) {
+            MovieDTO movieDTO = modelMapper.map(movieEntity, MovieDTO.class);
+            result.add(movieDTO);
+        }
+
+        return result;
+    }
+
+    @Override
+    public Optional<MovieDTO> findById(Long id) {
+        Optional<MovieEntity> movieEntityOptional = movieRepository.findById(id);
+
+        Optional<MovieDTO> movieDTO = movieEntityOptional.map(movieEntity -> modelMapper.map(movieEntity, MovieDTO.class));
+
+        return movieDTO;
+    }
+
+    @Override
+    public MovieDTO save(MovieDTO movieDTO) {
+        MovieEntity movieEntity = modelMapper.map(movieDTO, MovieEntity.class);
+        movieEntity.setId(null);
+
+        MovieEntity savedMovie = movieRepository.save(movieEntity);
+
+        return modelMapper.map(savedMovie, MovieDTO.class);
+    }
+
+    @Override
+    public MovieDTO update(MovieDTO movieDTO) {
+        Long id = movieDTO.getId();
+
+        boolean existsById = movieRepository.existsById(id);
+
+        if (existsById) {
+            MovieEntity movieToSave = modelMapper.map(movieDTO, MovieEntity.class);
+            MovieEntity savedMovie = movieRepository.save(movieToSave);
+            return modelMapper.map(savedMovie, MovieDTO.class);
+        } else {
+            throw new MovieNotFoundException("Movie not found with id " + id);
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        Optional<MovieEntity> optionalMovie = movieRepository.findById(id);
+
+        if (optionalMovie.isPresent()) {
+            movieRepository.delete(optionalMovie.get());
+        } else {
+            throw new MovieNotFoundException("Movie not found with id " + id);
+        }
+    }
+
+}
